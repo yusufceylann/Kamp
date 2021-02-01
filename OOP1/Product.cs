f64e210a3069f6174eb5701ac33ef14212f8209e
yusufceylann <yusufceylan259@gmail.com>
2021-02-01 14:32:33 +0300

    Create OOP1

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        //snippet
+        public int Id { get; set; } //Priöary Key
+        public int CategoryId { get; set; } //Foreign Key
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        // CRUD Operations (Create - Read - Update - Delete )
+
+    }
+}
