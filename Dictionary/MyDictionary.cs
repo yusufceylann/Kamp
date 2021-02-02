2f2a28d3dca2be91cc1359d11fd22c621983b47c
yusufceylann <yusufceylan259@gmail.com>
2021-02-02 06:37:28 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary <_key, _value> : Dictionary<_key, _value>
+    {
+        _key[] keys;
+        _value[] values;
+
+        // Constructor
+        public MyDictionary()
+        {
+            keys = new _key[0];
+            values = new _value[0];
+        }
+
+        
+        public void Add(_key key, _value value)
+        {
+            _key[] tempKey = keys;
+            _value[] tempValue = values;
+
+            keys = new _key[keys.Length + 1];
+            values = new _value[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public _key[] Keys
+        {
+            get { return keys;  }
+        }
+
+        public _value[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Ogrenci = new MyDictionary<int, string>();
+            Ogrenci.Add(138, "Tolga Demirer");
+            Ogrenci.Add(158, "Ümit Özkan");
+            Ogrenci.Add(115, "Kadir Aydemir");
+            Ogrenci.Add(174, "Cemal Çiftçi");
+
+            Console.WriteLine("Öğrenci No Giriniz: ");
+            int No = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(Ogrenci[No]);
+            }
+            catch
+            {
+                Console.WriteLine("Öğrenci Bulunamadı");
+            }
+
+        }
+    }
+}
