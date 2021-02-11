5761e266e96a69b5a00240a627e72bd9dc00c199
yusufceylann <yusufceylan259@gmail.com>
2021-02-11 15:57:19 +0300

    - Müşteri Kimlik Kontrolü

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Adapters;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            //BaseCustomerManager customerManager = new StarbuckscustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer {DateOfBirth = new DateTime(1985,1,6),FirstName="Engin",
+                LastName="Demiroğ", NationalityId="28861499108"});
+            Console.ReadLine();
+        }
+    }
+}
